c9a6631fa59ac8d91f792ca48df0a1f054744c07
Makasimchik <titaevmaks2005@gmail.com>
2026-09-16 23:03:37 +0300

    titaev_m_lab1_extra_task

--- clang/compiler-course/titaev_m_lab1/source.cpp
+++ clang/compiler-course/titaev_m_lab1/source.cpp
@@ -1,298 +1,60 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
-#include "clang/AST/ASTTypeTraits.h"
 #include "clang/AST/Decl.h"
-#include "clang/AST/Expr.h"
-#include "clang/AST/ParentMapContext.h"
 #include "clang/AST/RecursiveASTVisitor.h"
-#include "clang/AST/Stmt.h"
 #include "clang/Basic/Diagnostic.h"
-#include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
-#include "llvm/ADT/DenseMap.h"
 
 using namespace clang;
 
 namespace {
 
-enum class ResKind { NewAlloc, MallocAlloc, FopenAlloc };
-
-struct ResInfo {
-  ResKind kind;
-  SourceLocation where;
-  const VarDecl *decl;
-  bool isFreed = false;
-  bool alreadyReported = false;
-};
-
-class ResourceLeakVisitor final
-    : public RecursiveASTVisitor<ResourceLeakVisitor> {
+class DeprecatedFunctionVisitor final
+    : public RecursiveASTVisitor<DeprecatedFunctionVisitor> {
 public:
-  explicit ResourceLeakVisitor(ASTContext *ctx) : ctx(ctx) {}
+  explicit DeprecatedFunctionVisitor(ASTContext *Context) : Context(Context) {}
 
-  bool VisitFunctionDecl(FunctionDecl *fd) {
-    if (!fd->isThisDeclarationADefinition())
+  bool VisitFunctionDecl(FunctionDecl *FD) {
+    if (!FD->isThisDeclarationADefinition())
       return true;
 
-    StringRef name = fd->getName();
+    llvm::StringRef Name = FD->getName();
 
-    if (!name.contains("deprecated"))
+    if (!Name.contains("deprecated"))
       return true;
 
-    DiagnosticsEngine &DE = ctx->getDiagnostics();
-    unsigned id =
+    DiagnosticsEngine &DE = Context->getDiagnostics();
+    unsigned ID =
         DE.getCustomDiagID(DiagnosticsEngine::Warning,
                            "function '%0' contains 'deprecated' in its name");
 
-    DE.Report(fd->getLocation(), id) << name;
+    DE.Report(FD->getLocation(), ID) << Name;
 
     return true;
-  }
-
-  bool VisitVarDecl(VarDecl *vd) {
-    if (!vd->hasInit())
-      return true;
-
-    const Expr *init = vd->getInit();
-    if (!init)
-      return true;
-
-    init = init->IgnoreParenImpCasts();
-    init = init->IgnoreCasts();
-
-    if (const auto *ne = dyn_cast<CXXNewExpr>(init)) {
-      registerAlloc(vd, ResKind::NewAlloc, vd->getLocation());
-    } else if (const auto *call = dyn_cast<CallExpr>(init)) {
-      if (const FunctionDecl *fd = call->getDirectCallee()) {
-        StringRef nm = fd->getName();
-        if (nm == "malloc") {
-          registerAlloc(vd, ResKind::MallocAlloc, vd->getLocation());
-        } else if (nm == "fopen") {
-          registerAlloc(vd, ResKind::FopenAlloc, vd->getLocation());
-        }
-      }
-    }
-    return true;
-  }
-
-  bool VisitBinaryOperator(BinaryOperator *bo) {
-    if (!bo->isAssignmentOp())
-      return true;
-
-    const Expr *lhs = bo->getLHS();
-    const Expr *rhs = bo->getRHS();
-    if (!lhs || !rhs)
-      return true;
-
-    lhs = lhs->IgnoreParenImpCasts();
-    lhs = lhs->IgnoreCasts();
-    rhs = rhs->IgnoreParenImpCasts();
-    rhs = rhs->IgnoreCasts();
-
-    const DeclRefExpr *ref = dyn_cast<DeclRefExpr>(lhs);
-    if (!ref)
-      return true;
-
-    const VarDecl *var = dyn_cast<VarDecl>(ref->getDecl());
-    if (!var)
-      return true;
-
-    if (const auto *ne = dyn_cast<CXXNewExpr>(rhs)) {
-      registerAlloc(var, ResKind::NewAlloc, var->getLocation());
-    } else if (const auto *call = dyn_cast<CallExpr>(rhs)) {
-      if (const FunctionDecl *fd = call->getDirectCallee()) {
-        StringRef nm = fd->getName();
-        if (nm == "malloc") {
-          registerAlloc(var, ResKind::MallocAlloc, var->getLocation());
-        } else if (nm == "fopen") {
-          registerAlloc(var, ResKind::FopenAlloc, var->getLocation());
-        }
-      }
-    }
-    return true;
-  }
-
-  bool VisitCXXDeleteExpr(CXXDeleteExpr *del) {
-    const Expr *arg = del->getArgument();
-    if (!arg)
-      return true;
-
-    arg = arg->IgnoreParenImpCasts();
-    arg = arg->IgnoreCasts();
-
-    if (const DeclRefExpr *ref = dyn_cast<DeclRefExpr>(arg)) {
-      if (const VarDecl *var = dyn_cast<VarDecl>(ref->getDecl())) {
-        markAsFreed(var);
-      }
-    }
-    return true;
-  }
-
-  bool VisitCallExpr(CallExpr *call) {
-    if (const FunctionDecl *fd = call->getDirectCallee()) {
-      StringRef nm = fd->getName();
-      if (nm == "free" || nm == "fclose") {
-        if (call->getNumArgs() >= 1) {
-          const Expr *arg = call->getArg(0);
-          if (!arg)
-            return true;
-
-          arg = arg->IgnoreParenImpCasts();
-          arg = arg->IgnoreCasts();
-
-          if (const DeclRefExpr *ref = dyn_cast<DeclRefExpr>(arg)) {
-            if (const VarDecl *var = dyn_cast<VarDecl>(ref->getDecl())) {
-              markAsFreed(var);
-            }
-          }
-        }
-      }
-    }
-    return true;
-  }
-
-  const FunctionDecl *getParentFunction(const Stmt *st) {
-    if (!st)
-      return nullptr;
-
-    DynTypedNode node = DynTypedNode::create(*st);
-
-    while (true) {
-      auto parents = ctx->getParents(node);
-      if (parents.empty())
-        break;
-
-      node = parents[0];
-
-      if (const FunctionDecl *fd = node.get<FunctionDecl>())
-        return fd;
-    }
-    return nullptr;
-  }
-
-  bool VisitReturnStmt(ReturnStmt *rs) {
-    const Expr *ret = rs->getRetValue();
-
-    if (ret) {
-      ret = ret->IgnoreParenImpCasts();
-      ret = ret->IgnoreCasts();
-
-      if (const DeclRefExpr *ref = dyn_cast<DeclRefExpr>(ret)) {
-        if (const VarDecl *var = dyn_cast<VarDecl>(ref->getDecl())) {
-          auto it = allocations.find(var);
-          if (it != allocations.end() && !it->second.isFreed &&
-              !it->second.alreadyReported) {
-
-            DiagnosticsEngine &DE = ctx->getDiagnostics();
-            unsigned id = DE.getCustomDiagID(
-                DiagnosticsEngine::Warning,
-                "Ресурс для переменной '%0' может быть не освобожден (не "
-                "гарантированное освобождение при return)!");
-            DE.Report(rs->getBeginLoc(), id) << var->getName();
-
-            it->second.alreadyReported = true;
-          }
-        }
-      }
-    } else {
-      const FunctionDecl *fd = getParentFunction(rs);
-      if (!fd)
-        return true;
-
-      for (auto &pair : allocations) {
-        ResInfo &info = pair.second;
-
-        if (info.isFreed || info.alreadyReported)
-          continue;
-
-        const DeclContext *dc = info.decl->getDeclContext();
-        const DeclContext *cur = dc;
-
-        bool sameFunc = false;
-        while (cur) {
-          if (cur == fd) {
-            sameFunc = true;
-            break;
-          }
-          cur = cur->getParent();
-        }
-
-        if (!sameFunc)
-          continue;
-
-        SourceManager &SM = ctx->getSourceManager();
-
-        if (SM.isBeforeInTranslationUnit(info.decl->getLocation(),
-                                         rs->getBeginLoc())) {
-
-          DiagnosticsEngine &DE = ctx->getDiagnostics();
-          unsigned id = DE.getCustomDiagID(
-              DiagnosticsEngine::Warning,
-              "Ресурс для переменной '%0' может быть не освобожден (не "
-              "гарантированное освобождение при return)!");
-          DE.Report(rs->getBeginLoc(), id) << info.decl->getName();
-
-          info.alreadyReported = true;
-        }
-      }
-    }
-    return true;
-  }
-
-  void finalizeDiagnostics() {
-    DiagnosticsEngine &DE = ctx->getDiagnostics();
-
-    for (const auto &pair : allocations) {
-      const ResInfo &info = pair.second;
-
-      if (!info.isFreed && !info.alreadyReported) {
-        unsigned id = DE.getCustomDiagID(
-            DiagnosticsEngine::Warning,
-            "Память или ресурс для переменной '%0' не освобождены!");
-        DE.Report(info.where, id) << info.decl->getName();
-      }
-    }
   }
 
 private:
-  ASTContext *ctx;
-  llvm::DenseMap<const VarDecl *, ResInfo> allocations;
-
-  void registerAlloc(const VarDecl *vd, ResKind k, SourceLocation loc) {
-    ResInfo info;
-    info.kind = k;
-    info.where = loc;
-    info.decl = vd;
-    allocations[vd] = info;
-  }
-
-  void markAsFreed(const VarDecl *vd) {
-    auto it = allocations.find(vd);
-    if (it != allocations.end()) {
-      it->second.isFreed = true;
-    }
-  }
+  ASTContext *Context;
 };
 
-class ResourceLeakConsumer final : public ASTConsumer {
+class DeprecatedFunctionConsumer final : public ASTConsumer {
 public:
-  explicit ResourceLeakConsumer(ASTContext *ctx) : visitor(ctx) {}
+  explicit DeprecatedFunctionConsumer(ASTContext *Context) : Visitor(Context) {}
 
-  void HandleTranslationUnit(ASTContext &ctx) override {
-    visitor.TraverseDecl(ctx.getTranslationUnitDecl());
-    visitor.finalizeDiagnostics();
+  void HandleTranslationUnit(ASTContext &Context) override {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
   }
 
 private:
-  ResourceLeakVisitor visitor;
+  DeprecatedFunctionVisitor Visitor;
 };
 
-class ResourceLeakAction final : public PluginASTAction {
+class DeprecatedFunctionAction final : public PluginASTAction {
 public:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci,
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  llvm::StringRef) override {
-    return std::make_unique<ResourceLeakConsumer>(&ci.getASTContext());
+    return std::make_unique<DeprecatedFunctionConsumer>(&CI.getASTContext());
   }
 
   bool ParseArgs(const CompilerInstance &,
@@ -303,5 +65,6 @@ public:
 
 } // namespace
 
-static FrontendPluginRegistry::Add<ResourceLeakAction>
-    Z("leak_checker", "TU-level leak checker");
+static FrontendPluginRegistry::Add<DeprecatedFunctionAction>
+    X("deprecated_function_checker",
+      "warn about functions containing 'deprecated' in their name");
--- clang/test/compiler-course/titaev_m_lab1/test.cpp
+++ clang/test/compiler-course/titaev_m_lab1/test.cpp
@@ -1,106 +1,18 @@
-// leak_tests.cpp
-// RUN: split-file %s %t
-// RUN: %clang_cc1 -load %llvmshlibdir/titaev_m_lab1_ClangAST%pluginext -plugin leak_checker -fsyntax-only -verify %t/with_warnings.cpp
-// RUN: %clang_cc1 -load %llvmshlibdir/titaev_m_lab1_ClangAST%pluginext -plugin leak_checker -fsyntax-only -verify %t/without_warnings.cpp
-
-//--- with_warnings.cpp
-extern "C" {
-    void* malloc(unsigned long size);
-    void* fopen(const char* filename, const char* mode);
-}
-
-// Дополнительное задание:
-// warning должен появляться только для определения функции,
-// в имени которой содержится "deprecated".
+// RUN: %clang_cc1 -load %llvmshlibdir/titaev_m_lab1_ClangAST%pluginext -plugin deprecated_function_checker -fsyntax-only -verify %s
 
 void deprecated_declared_only();
 
-void deprecated_function() {} // expected-warning {{function 'deprecated_function' contains 'deprecated' in its name}}
+void deprecated_function() {}
+// expected-warning@-1 {{function 'deprecated_function' contains 'deprecated' in its name}}
 
-int my_deprecated_api(int value) { // expected-warning {{function 'my_deprecated_api' contains 'deprecated' in its name}}
+int my_deprecated_api(int value) {
   return value;
 }
+// expected-warning@-3 {{function 'my_deprecated_api' contains 'deprecated' in its name}}
 
 void normal_function() {}
 
-int* g_leak = (int*)malloc(100); // expected-warning {{Память или ресурс для переменной 'g_leak' не освобождены!}}
-
-int* return_malloc_leak(int n) {
-    int* q = (int*)malloc(n);
-    return q; // expected-warning {{Ресурс для переменной 'q' может быть не освобожден (не гарантированное освобождение при return)!}}
+int deprecated_inside_name() {
+  return 42;
 }
-
-int* return_new_leak(int n) {
-    int* r = new int[n];
-    return r; // expected-warning {{Ресурс для переменной 'r' может быть не освобожден (не гарантированное освобождение при return)!}}
-}
-
-void* fopen_return_leak(const char* path) {
-    void* fh = fopen(path, "r");
-    return fh; // expected-warning {{Ресурс для переменной 'fh' может быть не освобожден (не гарантированное освобождение при return)!}}
-}
-
-void early_exit_leak(int n, bool cond) {
-    int* arr = new int[n];
-    if (cond) {
-        return; // expected-warning {{Ресурс для переменной 'arr' может быть не освобожден (не гарантированное освобождение при return)!}}
-    }
-    delete[] arr;
-}
-
-void nested_scope_leak(int n) {
-    {
-        {
-            int* nested_ptr = (int*)malloc(n); // expected-warning {{Память или ресурс для переменной 'nested_ptr' не освобождены!}}
-        }
-    }
-}
-
-void missing_free_simple(int n) {
-    int* local_p = (int*)malloc(n); // expected-warning {{Память или ресурс для переменной 'local_p' не освобождены!}}
-}
-
-void missing_delete_simple(int n) {
-    int* local_arr = new int[n]; // expected-warning {{Память или ресурс для переменной 'local_arr' не освобождены!}}
-}
-
-//--- without_warnings.cpp
-// expected-no-diagnostics
-extern "C" {
-    void* malloc(unsigned long size);
-    void free(void* ptr);
-    void* fopen(const char* filename, const char* mode);
-    int fclose(void* stream);
-}
-
-void clean_malloc(int n) {
-    int* p = (int*)malloc(n);
-    free(p);
-}
-
-void clean_new(int n) {
-    int* a = new int[n];
-    delete[] a;
-}
-
-void clean_fopen(const char* path) {
-    void* f = fopen(path, "r");
-    fclose(f);
-}
-
-void clean_nested(int n) {
-    {
-        double* d = (double*)malloc(n);
-        free(d);
-    }
-}
-
-bool clean_branching(int n, int x) {
-    int* buf = new int[n];
-    if (n > x) {
-        delete[] buf;
-        return true;
-    }
-    delete[] buf;
-    return false;
-}
+// expected-warning@-3 {{function 'deprecated_inside_name' contains 'deprecated' in its name}}
